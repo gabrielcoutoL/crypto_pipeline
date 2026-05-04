2017c8048a7721d1de9aa145482e49d02688c456
gabrielcoutoL <79060224+gabrielcoutoL@users.noreply.github.com>
2026-05-03 21:03:33 -0300

    configuracoes do db postgres criadas e imagem do postgres adicionada no yaml

--- src/main.py
+++ src/main.py
@@ -1,12 +1,32 @@
 import logging
+from datetime import datetime
 from pathlib import Path
 
+from sqlalchemy import text
+
+from src.database import DATABASE_URL, engine
 from src.extract import CryptoAPIClient
 from src.transform import CryptoProcessor
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
+
+
+def criar_tabela_execs(engine_db):
+
+    with engine_db.begin() as conn:
+        conn.execute(
+            text(
+                "CREATE TABLE IF NOT EXISTS execucoes_etl (id SERIAL PRIMARY KEY, data_execucao TIMESTAMP, status VARCHAR(50))"
+            )
+        )
+
+        query_insert = text(
+            "INSERT INTO execucoes_etl (data_execucao, status) VALUES (:data_now, :status)"
+        )
+
+        conn.execute(query_insert, {"data_now": datetime.now(), "status": "SUCESSO"})
 
 
 # Lista de criptos com 2 moedas falsas pra simular erro
@@ -21,6 +41,9 @@ def main():
         "LINK",
         "ASSET_FALSO_2",
     ]
+
+    logging.info("Conectando no banco e registrando a execução.")
+    criar_tabela_execs(engine)
 
     logger = logging.getLogger(__name__)
     logger.info("Iniciando pipeline de extração de criptoativos...")
@@ -45,6 +68,13 @@ def main():
         # Salva o arquivo em parquet
         dados_transformados.write_parquet(destino)
         logger.info(f"Pipeline finalizado com sucesso! Arquivo salvo em: {destino}")
+
+        dados_transformados.write_database(
+            table_name="tb_crypto_metrics",
+            connection=DATABASE_URL,
+            if_table_exists="replace",
+        )
+        logger.info("Dados salvos no banco de dados.")
     else:
         logger.warning("Nenhum dado válido foi retornado pela API. Pipeline abortado.")
 
